82a774087eeb4bfa2066a374a3c7a7935b25458f
Knut Hühne <knut.huehne@futurice.com>
2018-12-17 17:02:30 +0100

    create function to calculate sums

--- sum.js
+++ sum.js
@@ -1,4 +1,8 @@
 const first = parseInt(process.argv[2]);
 const second = parseInt(process.argv[3]);
-const sum = first + second;
+const sum = sumArray([first, second]);
 console.log(sum);
+
+function sumArray(array){
+	return array.reduce((value, current) => current+value, 0)
+}
